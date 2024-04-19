4c484e5127d5c92a1b997e61b655cc34cdc9ab16
Máyra <mayrazucaro@gmail.com>
2024-04-19 10:17:50 -0300

    feat add track length without symbolic number

--- removeTripleElements.java
+++ removeTripleElements.java
@@ -1,7 +1,7 @@
 class RemoveTripleElements {
     public int removeDuplicates(int[] nums) {
         byte repeated = 0;
-
+        
         // Check if the element appears more than 3 times, if so asign a symbolic value of -99999 to filter this value
         for ( int i = 0; i < nums.length; i++ ) {
             for ( int x = 0; x < nums.length; x++ ) {
@@ -22,4 +22,29 @@ class RemoveTripleElements {
 
         return nums.length;       
     }
+
+    // Just track the length without taking care of the array's content
+    public int removeDuplicatesWithOutSymbolicNum(int[] nums) {
+        int length = 0;
+        int repeated = 0;
+        
+
+        for (int i = 0; i < nums.length; i++) {
+            // Check if the element is the firt element or if it is different of the previous number
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                // First time this number appear and increase the length
+                repeated = 1;
+                length++;
+            } else {
+                // Number is equals to the previous number
+                repeated++;
+                // If this number appeared less than 3 times in the array, increase the length
+                if (repeated <= 2) {
+                    length++;
+                }
+            }
+        }
+    
+        return length;
+	}  
 }
